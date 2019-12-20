995568e2b6ca977c78d0a6d5e57825937fe5d47a
Anthony Golovingit config --global user.name Anthony <AGOLOVIN97@live.com>
2019-12-20 14:05:36 -0800

    Add more test cases for more scenarios

--- WordCounter.Tests/Model.Tests/WordCounter.Tests.cs
+++ WordCounter.Tests/Model.Tests/WordCounter.Tests.cs
@@ -21,16 +21,32 @@ namespace WordCounterLogic.Tests
             RepeatCounter newRepeatCounter = new RepeatCounter();
             Assert.AreEqual(5,newRepeatCounter.UserInput("long john silvers LONG loNG LonG LoNg", "long"));
             Assert.AreEqual(4,newRepeatCounter.UserInput("pong pong pong long tong pong", "PONG"));
-            Assert.AreEqual(2,newRepeatCounter.UserInput("QERQGFEWR34355GVDFGBDDGFfgegdfdfg", "5"));
+            Assert.AreEqual(2,newRepeatCounter.UserInput("QERQGFEWR34355GVDFGBDDGFfgegdfdfg 555 5 555 5", "5"));
 
         } 
         
         [TestMethod]
-        public void UserInput_testtest_ReturnsNumberOfDuplicates()
+        public void UserInput_TestContiniousString_ReturnsNumberOfDuplicates()
         {
             RepeatCounter newRepeatCounter = new RepeatCounter();
-            Assert.AreEqual(2,newRepeatCounter.UserInput("pew pew words", "pew"));
-            Assert.AreEqual(3,newRepeatCounter.UserInput("yung thug was yung when he was yung", "yung"));
+            Assert.AreEqual(0,newRepeatCounter.UserInput("johnjohnjohnjohn", "john"));
+            Assert.AreEqual(1,newRepeatCounter.UserInput("bosssauceboss wow boss", "boss"));
+        }
+
+        [TestMethod]
+        public void UserInput_DoubleSpace_ReturnsNumberOfDuplicates()
+        {
+            RepeatCounter newRepeatCounter = new RepeatCounter();
+            Assert.AreEqual(2,newRepeatCounter.UserInput("log  log  dog", "log"));
+            Assert.AreEqual(1,newRepeatCounter.UserInput("t h i n g t,h,i,n,g  thing  gniht", "thing"));
+        }
+
+        [TestMethod]
+        public void UserInput_Punctuation_ReturnsNumberOfDuplicates()
+        {
+            RepeatCounter newRepeatCounter = new RepeatCounter();
+            Assert.AreEqual(1,newRepeatCounter.UserInput("Gus got off the bus, but he actually didnt", "bus"));
+            Assert.AreEqual(1,newRepeatCounter.UserInput("There once was a barnacle so ugly that everyone died, the end.", "end"));
         }
         
     }
